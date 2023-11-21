79b2c3b5eb06de17d3f33dd3bbad5bd5a73ff36c
ItsmeBlackOps <harikrushna09@gmail.com>
2023-11-21 08:31:45 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginComponent from './components/LoginComponent';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import DashboardComponent from './components/DashboardComponent';
 import TaskManagementComponent from './components/TaskManagementComponent';
 import InterviewSchedulingComponent from './components/InterviewSchedulingComponent';
 import FeedbackComponent from './components/FeedbackComponent';
 import CandidateManagementComponent from './components/CandidateManagementComponent';
-import LoginComponent from './components/LoginComponent';
+import StatisticsComponent from './components/StatisticsComponent';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
 import { NotificationProvider } from './Contexts/NotificationContext';
 
 const App = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const auth = getAuth();
+
+    useEffect(() => {
+        // Listen for auth state changes
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in.
+                setIsAuthenticated(true);
+            } else {
+                // User is signed out.
+                setIsAuthenticated(false);
+            }
+        });
+
+        // Cleanup subscription on unmount
+        return () => unsubscribe();
+    }, [auth]);
+
+
+    const handleLoginSuccess = () => {
+        setIsAuthenticated(true);
+    };
+
+    const handleLogout = () => {
+        setIsAuthenticated(false);
+        // Here you should also handle the logout logic with your authentication service
+    };
+
     return (
         <Router>
             <NotificationProvider>
-                <Header />
-                <Navigation />
+                {isAuthenticated && <Header onLogout={handleLogout} />}
+                {isAuthenticated && <Navigation />}
                 <div className="main-content">
                     <Routes>
-                        <Route path="/" element={<DashboardComponent />} />
-                        <Route path="/login" element={<LoginComponent />} />
-                        <Route path="/dashboard" element={<DashboardComponent />} />
-                        <Route path="/task-management" element={<TaskManagementComponent />} />
-                        <Route path="/interview-scheduling" element={<InterviewSchedulingComponent />} />
-                        <Route path="/feedback" element={<FeedbackComponent />} />
-                        <Route path="/candidate-management" element={<CandidateManagementComponent />} />
-                        {/* Add additional routes as necessary */}
+                        <Route path="/" element={isAuthenticated ? <Navigate replace to="/dashboard" /> : <LoginComponent onLoginSuccess={handleLoginSuccess} />} />
+                        <Route path="/dashboard" element={isAuthenticated ? <DashboardComponent /> : <Navigate replace to="/" />} />
+                        <Route path="/task-management" element={isAuthenticated ? <TaskManagementComponent /> : <Navigate replace to="/" />} />
+                        <Route path="/interview-scheduling" element={isAuthenticated ? <InterviewSchedulingComponent /> : <Navigate replace to="/" />} />
+                        <Route path="/feedback" element={isAuthenticated ? <FeedbackComponent /> : <Navigate replace to="/" />} />
+                        <Route path="/candidate-management" element={isAuthenticated ? <CandidateManagementComponent /> : <Navigate replace to="/" />} />
+                        <Route path="/statistics" element={isAuthenticated ? <StatisticsComponent /> : <Navigate replace to="/" />} />
+                        {/* Redirect any unknown routes to the dashboard, or a 404 component */}
+                        <Route path="*" element={<Navigate replace to={isAuthenticated ? "/dashboard" : "/"} />} />
                     </Routes>
                 </div>
-                <Footer />
+                {isAuthenticated && <Footer />}
             </NotificationProvider>
         </Router>
     );
--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,42 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom'; // assuming you're using react-router for navigation
+import { useNavigate } from 'react-router-dom';
+import '../../src/LoginComponent.css'; // Make sure the path is correct
+
 
 const LoginComponent = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState(null);
-    const history = useNavigate();
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
     const auth = getAuth();
 
+    useEffect(() => {
+        // This function should only set up the listener once
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            if (user) {
+                console.log("User is signed in, navigating to /dashboard");
+                navigate('/dashboard');
+            } else {
+                console.log("User is signed out");
+            }
+        });
+    
+        // Cleanup subscription on unmount
+        return () => unsubscribe(); // Make sure to call unsubscribe function
+    }, [navigate]);
+    
     const handleLogin = async (e) => {
         e.preventDefault();
-        setError(null);
+        setError('');
 
         try {
-            await signInWithEmailAndPassword(auth, email, password);
-            history.push('/dashboard'); // redirect to the dashboard
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            console.log('Logged in user:', userCredential.user);
+            // No need to navigate here as the useEffect will handle it
         } catch (error) {
+            console.error('Error during login:', error);
             setError('Login failed: ' + error.message);
         }
     };
 
     return (
-        <div>
-            <h1>Login</h1>
-            {error && <div style={{ color: 'red' }}>{error}</div>}
-            <form onSubmit={handleLogin}>
-                <label>
-                    Email:
-                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-                </label>
-                <br />
-                <label>
-                    Password:
-                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-                </label>
-                <br />
-                <button type="submit">Login</button>
+        <div className="login-container">
+            <form className="login-form" onSubmit={handleLogin}>
+                <h1>Login</h1>
+                {error && <p className="error-message">{error}</p>}
+                <div className="input-field">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="input-field">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit" className="login-button">Login</button>
             </form>
         </div>
     );
